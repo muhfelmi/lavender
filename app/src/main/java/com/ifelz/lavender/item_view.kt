dccdda9b2717247b65e0700fc62885e9019828ea
Muhammad Felmi <felmiadi@gmail.com>
2021-03-05 14:18:54 +0700

    Minor Fix

--- app/src/main/java/com/ifelz/lavender/item_view.kt
+++ app/src/main/java/com/ifelz/lavender/item_view.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class item_view : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_item_view)
+        setContentView(R.layout.item_view)
     }
 }
