1a4966d68338ea8b18f17a185d105d11ee71bcc0
Muhammad Felmi <felmiadi@gmail.com>
2021-03-05 14:17:15 +0700

    Added dashboard

---
+++ app/src/main/java/com/ifelz/lavender/item_view.kt
@@ -0,0 +1,11 @@
+package com.ifelz.lavender
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class item_view : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_view)
+    }
+}
