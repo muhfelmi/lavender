2a3958bb9e9439a293dfc088091be9a47dada034
Muhammad Felmi <felmiadi@gmail.com>
2021-03-05 15:59:56 +0700

    Fixxxxxxxes

--- app/src/main/java/com/ifelz/lavender/LoginActivity.kt
+++ app/src/main/java/com/ifelz/lavender/LoginActivity.kt
@@ -2,10 +2,19 @@ package com.ifelz.lavender
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+      
     }
 }
