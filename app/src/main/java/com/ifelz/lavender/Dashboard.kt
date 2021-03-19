38b4b6171d2babdc787a1724d216269309a8be13
Muhammad Felmi <felmiadi@gmail.com>
2021-03-19 13:39:19 +0700

    Updated Code

--- app/src/main/java/com/ifelz/lavender/Dashboard.kt
+++ app/src/main/java/com/ifelz/lavender/Dashboard.kt
@@ -2,28 +2,47 @@ package com.ifelz.lavender
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_dashboard.*
 import okhttp3.*
 import java.io.IOException
-
 class Dashboard : AppCompatActivity() {
 
-    val client = OkHttpClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        run("https://api.openweathermap.org/data/2.5/forecast?q=purwakarta,ID&appid=2ce659b9c25fc6fe3a07de4ca71d1dac")
+        //run("https://api.openweathermap.org/data/2.5/forecast?q=purwakarta,ID&appid=2ce659b9c25fc6fe3a07de4ca71d1dac")
+
+        NetworkConfig().getService().getWeathers("purwakarta,ID", "2ce659b9c25fc6fe3a07de4ca71d1dac").enqueue(object : retrofit2.Callback<ResultWeather> {
+
+            override fun onResponse(call: retrofit2.Call<ResultWeather>, response: retrofit2.Response<ResultWeather>) {
+                //var item = response.body()
+                Log.d("response", response.body().toString())
+
+                //txtKota.setText(item.city.name)
+            }
+
+            override fun onFailure(call: retrofit2.Call<ResultWeather>, t: Throwable) {
+                Log.d("error response service", t.message.toString())
+
+                Toast.makeText(applicationContext, t.message.toString(), Toast.LENGTH_LONG).show()
+            }
+        })
+
     }
 
-    fun run(url: String) {
+    fun run(url: String){
+        val client = OkHttpClient()
         val request = Request.Builder()
-            .url(url)
-            .build()
+                .url(url)
+                .build()
 
-        client.newCall(request).enqueue(object : Callback{
+        client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {}
             override fun onResponse(call: Call, response: Response) = println(response.body()?.string())
         })
     }
-
 }
