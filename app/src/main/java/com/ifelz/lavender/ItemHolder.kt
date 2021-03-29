57eb837a523397a16fd00baedeb442c8fa062200
Muhammad Felmi <felmiadi@gmail.com>
2021-03-29 16:55:17 +0700

    Minor Bug Fixes

--- app/src/main/java/com/ifelz/lavender/ItemHolder.kt
+++ app/src/main/java/com/ifelz/lavender/ItemHolder.kt
@@ -10,7 +10,7 @@ class ItemHolder(view: View) : RecyclerView.ViewHolder(view) {
     private val description = itemView.desc
     private val tempNow = itemView.temp
     private val tempMinMax = itemView.temp_range
-    private val imageIcon = itemView.image_desc
+    private val imageIcon = itemView.icon
 
     fun bindItem(item: ListItem) {
         dateTime.text = item.dt?.let { Util.getDayName(it) }
@@ -18,6 +18,7 @@ class ItemHolder(view: View) : RecyclerView.ViewHolder(view) {
         tempNow.text = item.main?.temp?.let { Util.setFormatTemperature(it) }
         tempMinMax.text = item?.main?.tempMin?.let { Util.setFormatTemperature(it) } + " - " + item.main?.tempMax?.let { Util.setFormatTemperature(it) }
         //item?.weather?.get(0)?.id?.let { Util.getArtResourceForWeatherCondition(it) }?.let { imageIcon.setImageResource(it) }
-        //weatherId?.let { Util.getArtResourceForWeatherCondition(it) }?.let { imageIcon.setImageResource(it) }
+        val weatherId = item.weather?.get(0)?.id
+        weatherId?.let { Util.getArtResourceForWeatherCondition(it) }?.let { imageIcon.setImageResource(it) }
     }
 }
